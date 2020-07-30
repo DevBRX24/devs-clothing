7859bd5780061b1d03f2f31c6d14bfa4c79a09d0
DevBriX <45864249+DevBRX24@users.noreply.github.com>
2020-07-30 22:10:49 +0800

    Implemented memoized selectors for selecting cart items, and update cart count with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.components';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 import './cart-dropdown.styles.scss';
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem}></CartItem>
+      ))}
       <CustomButton>CHECKOUT</CustomButton>
     </div>
   </div>
 );
 
-export default CartDropDown;
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+});
+
+export default connect(mapStateToProps)(CartDropDown);
