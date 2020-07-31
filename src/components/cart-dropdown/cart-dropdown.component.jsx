1497645cd255829752a4bedf422039d6c5b9a1a9
DevBriX <45864249+DevBRX24@users.noreply.github.com>
2020-07-31 21:40:12 +0800

    added checkout page and updated cart-dropdown, header, user, cart and used selectors for getting data as well as for performance purposes

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.components';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropDown = ({ cartItems }) => (
+const CartDropDown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem}></CartItem>
-      ))}
-      <CustomButton>CHECKOUT</CustomButton>
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem}></CartItem>
+        ))
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )}
+      <CustomButton
+        onClick={() => {
+          history.push('/checkout');
+          dispatch(toggleCartHidden());
+        }}
+      >
+        CHECKOUT
+      </CustomButton>
     </div>
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  cartItems: selectCartItems(state),
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
 });
 
-export default connect(mapStateToProps)(CartDropDown);
+export default withRouter(connect(mapStateToProps)(CartDropDown));
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,6 +1,6 @@
 import CartActionTypes from './cart.types';
 
-const toggleCartHidden = () => ({
+export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
 });
 
